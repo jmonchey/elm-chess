78b1f53e26d8f6b51846384196f8b2084ad03fd6
John Ojong-moebot <johno@resrequest.com>
2019-07-09 21:03:16 +0200

    Implement initial model of the game

--- src/Main.elm
+++ src/Main.elm
@@ -8,13 +8,110 @@ import Html exposing (..)
 -- Model
 
 
+type Colour
+    = White
+    | Black
+
+
+type PieceType
+    = King
+    | Queen
+    | Rook
+    | Bishop
+    | Knight
+    | Pawn
+
+
+type alias Row =
+    Int
+
+
+type alias Column =
+    Int
+
+
+type alias Piece =
+    { colour : Colour
+    , type_ : PieceType
+    }
+
+
+type alias Square =
+    { row : Row
+    , column : Column
+    , piece : Maybe Piece
+    }
+
+
+type alias Board =
+    List Square
+
+
+type GameState
+    = PlayerToMove Colour
+    | Win Colour
+    | Draw
+
+
 type alias Model =
-    String
+    { board : Board
+    , state : GameState
+    }
 
 
-initialModel : Model
-initialModel =
-    "Welcome to Elm Chess!"
+reset : Model
+reset =
+    Model initialBoard initialState
+
+
+initialBoard : Board
+initialBoard =
+    rowOfPieces 1 White
+        ++ rowOfPawns 2 White
+        ++ emptyRows 3 6
+        ++ rowOfPawns 7 Black
+        ++ rowOfPieces 8 Black
+
+
+rowOfPieces : Row -> Colour -> List Square
+rowOfPieces row colour =
+    [ Square row 1 (Just <| Piece colour Rook)
+    , Square row 2 (Just <| Piece colour Knight)
+    , Square row 3 (Just <| Piece colour Bishop)
+    , Square row 4 (Just <| Piece colour Queen)
+    , Square row 5 (Just <| Piece colour King)
+    , Square row 6 (Just <| Piece colour Bishop)
+    , Square row 7 (Just <| Piece colour Knight)
+    , Square row 8 (Just <| Piece colour Rook)
+    ]
+
+
+rowOfPawns : Row -> Colour -> List Square
+rowOfPawns row colour =
+    List.range 1 8
+        |> List.map (pawnSquare colour row)
+
+
+pawnSquare : Colour -> Row -> Column -> Square
+pawnSquare colour row column =
+    Square row column (Just <| Piece colour Pawn)
+
+
+emptyRows : Row -> Row -> List Square
+emptyRows rowStart rowEnd =
+    List.range rowStart rowEnd
+        |> List.concatMap emptyRow
+
+
+emptyRow : Row -> List Square
+emptyRow row =
+    List.range 1 8
+        |> List.map (\column -> Square row column Nothing)
+
+
+initialState : GameState
+initialState =
+    PlayerToMove White
 
 
 
@@ -37,7 +134,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ text model ]
+        []
 
 
 
@@ -47,7 +144,7 @@ view model =
 main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = initialModel
+        { init = reset
         , update = update
         , view = view
         }
